087f2b064c8a914a7692ef8fc5fc1ba1c72f78fd
Arunesh Tiwari <77105301+Arunesh-Tiwari@users.noreply.github.com>
2023-03-31 00:40:07 +0530

    Time: 49 ms (50.25%), Space: 14.2 MB (13.16%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,14 @@
+class Solution:
+    """
+    Time:   O(2^n)
+    Memory: O(2^n)
+    """
+
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return '1'
+        return self.make_count(self.countAndSay(n - 1))
+
+    @staticmethod
+    def make_count(n: str) -> str:
+        return ''.join(f'{sum(1 for _ in gr)}{key}' for key, gr in groupby(n))
