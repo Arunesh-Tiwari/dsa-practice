101276407f8de434c9fddc5056d0a12ca055775a
Arunesh Tiwari <77105301+Arunesh-Tiwari@users.noreply.github.com>
2023-05-21 04:00:06 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.py
+++ Rod Cutting - GFG/rod-cutting.py
@@ -3,23 +3,41 @@
 class Solution:
     def cutRod(self, price, N):
         #code here
-        dp = {}
-        def solve(cl, rl):
-            if cl==0 or rl==0:
-                return 0
-            elif (cl,rl) in dp:
-                return dp[(cl,rl)]
-            else:
-                val = price[cl-1]
-                if cl<=rl:
-                    c1 = val+ solve(cl,rl-cl)
-                    c2 = solve(cl-1,rl)
-                    c = max(c1,c2)
+        dp = [[0]*(N+1) for _ in range(N+1)]
+        
+        for i in range(N+1):
+            for j in range(N+1):
+                cl = i
+                rl = j
+                if cl==0 or rl==0:
+                    dp[i][j] = 0
                 else:
-                    c = solve(cl-1,rl)
-                dp[(cl,rl)] = c
-                return c
-        return solve(N,N)
+                    val = price[cl-1]
+                    if cl<=rl:
+                        c1 = val+dp[cl][rl-cl]
+                        c2 = dp[cl-1][rl]
+                        dp[i][j] = max(c1,c2)
+                    else:
+                        dp[i][j] = dp[cl-1][rl]
+        return dp[N][N]
+        # dp = {}
+        # def solve(cl, rl):
+        #     if cl==0 or rl==0:
+        #         return 0
+        #     elif (cl,rl) in dp:
+        #         return dp[(cl,rl)]
+        #     else:
+        #         val = price[cl-1]
+        #         if cl<=rl:
+        #             c1 = val+ solve(cl,rl-cl)
+        #             c2 = solve(cl-1,rl)
+        #             c = max(c1,c2)
+        #         else:
+        #             c = solve(cl-1,rl)
+        #         dp[(cl,rl)] = c
+        #         return c
+        # return solve(N,N)
+        
             
 
 
