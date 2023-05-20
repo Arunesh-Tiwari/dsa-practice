1ac0216085bb574cbde36427b936f4b4ff1fac3c
Arunesh Tiwari <77105301+Arunesh-Tiwari@users.noreply.github.com>
2023-05-21 03:33:25 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.py
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.py
@@ -3,42 +3,22 @@ import sys
 sys.setrecursionlimit(5000)
 class Solution:
     def knapSack(self, N, W, val, wt):
-        # code here
-        # dp = {}
-        # def solve(n,cap):
-        #     if n==0 or cap==0:
-        #         return 0
-        #     elif (n,cap) in dp:
-        #         return dp[(n,cap)]
-        #     else:
-        #         cw = wt[n-1]
-        #         cv = val[n-1]
-        #         if cw<=cap:
-        #             c1 = cv+solve(n,cap-cw)
-        #             c2 = solve(n-1,cap)
-        #             c = max(c1,c2)
-        #         else:
-        #             c = solve(n-1,cap)
-        #         dp[(n,cap)] = c
-        #         return c
-        # return solve(N,W)
-        
-        dp = [[0]*(W+1) for _ in range(N)]
+        dp = [0]*(W+1)
         
         for i in range(N):
             for j in range(W+1):
-                cap = j
+                cap = j 
                 cv = val[i]
                 cw = wt[i]
                 
                 if i==0:
-                    dp[i][j] = (cap//cw)*cv
+                    dp[j] = (cap//cw)*cv
                 else:
-                    if cw<= cap:
-                        dp[i][j] = max(cv+dp[i][cap-cw],dp[i-1][cap])
+                    if cw<=cap:
+                        dp[j] = max(cv+dp[cap-cw],dp[cap])
                     else:
-                        dp[i][j] = dp[i-1][cap]
-        return dp[N-1][W]
+                        dp[j] = dp[cap]
+        return dp[W]
         
         
 
