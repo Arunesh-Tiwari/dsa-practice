6beee0d61caddddf808e0b0b6a0f46a07cfd5bdf
Arunesh Tiwari <77105301+Arunesh-Tiwari@users.noreply.github.com>
2023-05-21 17:43:43 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.py
+++ Longest Common Subsequence - GFG/longest-common-subsequence.py
@@ -4,24 +4,40 @@ class Solution:
     
     #Function to find the length of longest common subsequence in two strings.
     def lcs(self,x,y,str1,str2):
+        dp = [[0]*(y+1) for _ in range(x+1)]
+        
+        for i in range(x+1):
+            for j in range(y+1):
+                m = i
+                n = j
+                if m==0 or n==0:
+                    dp[i][j] = 0
+                else:
+                    if str1[m-1]==str2[n-1]:
+                        dp[i][j] = 1+dp[m-1][n-1]
+                    else:
+                        c1 = dp[m-1][n]
+                        c2 = dp[m][n-1]
+                        dp[i][j] = max(c1,c2)
+        return dp[x][y]
         
         # code here
-        dp = {}
-        def solve(s1,s2,m,n):
-            if m==0 or n==0:
-                return 0
-            elif (m,n) in dp:
-                return dp[(m,n)]
-            else:
-                if s1[m-1]==s2[n-1]:
-                    c = 1+solve(s1,s2,m-1,n-1)
-                else:
-                    c1 = solve(s1,s2,m-1,n)
-                    c2 = solve(s1,s2,m,n-1)
-                    c = max(c1,c2)
-                dp[(m,n)] = c
-                return c
-        return solve(str1,str2,x,y)
+        # dp = {}
+        # def solve(s1,s2,m,n):
+        #     if m==0 or n==0:
+        #         return 0
+        #     elif (m,n) in dp:
+        #         return dp[(m,n)]
+        #     else:
+        #         if s1[m-1]==s2[n-1]:
+        #             c = 1+solve(s1,s2,m-1,n-1)
+        #         else:
+        #             c1 = solve(s1,s2,m-1,n)
+        #             c2 = solve(s1,s2,m,n-1)
+        #             c = max(c1,c2)
+        #         dp[(m,n)] = c
+        #         return c
+        # return solve(str1,str2,x,y)
 
 
 #{ 
