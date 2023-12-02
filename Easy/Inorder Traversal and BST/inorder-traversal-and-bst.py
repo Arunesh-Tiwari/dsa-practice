4d14fffe46a61eb0fcdabe31279415d219da680e
Arunesh Tiwari <77105301+Arunesh-Tiwari@users.noreply.github.com>
2023-12-02 19:50:47 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Inorder Traversal and BST/inorder-traversal-and-bst.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+
+class Solution:
+    def isRepresentingBST(self, arr, N):
+        # code here
+        for i in range(N):
+            if arr[i]==arr[-1]:
+                break
+            if arr[i]>arr[i+1]:
+                return 0
+        return 1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range (t):
+        N = int(input())
+        arr = input().split()
+        for itr in range(N):
+            arr[itr] = int(arr[itr])
+        
+        ob = Solution()
+        print(ob.isRepresentingBST(arr, N))
+# } Driver Code Ends
